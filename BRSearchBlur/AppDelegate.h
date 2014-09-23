4a778e7000fe1d8c373f5f32c9d53008c8552fc1
Aaron Stephenson <aaron@bronron.com>
2014-09-23 11:01:15 +1000

    Initial Commit

---
+++ BRSearchBlur/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BRSearchBlur
+//
+//  Created by Aaron Stephenson on 23/09/2014.
+//  Copyright (c) 2014 Bronron Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BRSearchBlur/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BRSearchBlur
+//
+//  Created by Aaron Stephenson on 23/09/2014.
+//  Copyright (c) 2014 Bronron Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
